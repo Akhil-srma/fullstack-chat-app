efee77474752dead120b95ea4a23520e31dadb3b
Akhil Sharma <akhilsharma0321k@gmail.com>
2025-10-15 13:14:51 +0530

    Initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// Base URL
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api" // local backend
+  : "https://chat-app-e1ns.onrender.com/api"; // deployed backend
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
